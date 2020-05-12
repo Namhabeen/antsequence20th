76dc91cf7480d034355961ed2b83697b775efee4
South_under_been <s2018s27@e-mirim.hs.kr>
2020-05-12 22:54:15 +0900

    05월 12일 수업 08번

--- antsequence20th/Program.cs
+++ antsequence20th/Program.cs
@@ -10,6 +10,33 @@ namespace antsequence20th
     {
         static void Main(string[] args)
         {
+            string suyeol = "1";
+            int n = 1;
+            while (n <= 20)
+            {
+
+                Console.WriteLine(n + "번째 수열 : " + suyeol);
+                char token = suyeol[0];
+                int count = 0;
+                string temp_suyeol = "";
+                for (int i = 0; i < suyeol.Length; i++)
+                {
+                    if (token == suyeol[i])
+                        count++;
+                    else
+                    {
+                        temp_suyeol += "" + token + count;
+                        count = 0;
+                        token = suyeol[i];
+                        count++;
+                    }
+                    if (i + 1 == suyeol.Length)
+                        temp_suyeol += "" + token + count;
+                }
+                suyeol = temp_suyeol;
+                n++;
+            }
+
         }
     }
 }
